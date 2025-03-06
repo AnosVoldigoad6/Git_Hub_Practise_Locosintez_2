6b77d4e03b3c5f4f3c1bee773bd2c8fb9590ac8e
Shohdidor <shohdidorkhojaev@gmail.com>
2025-03-06 09:28:15 +0500

    page About is done

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,16 +1,19 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { Search } from 'lucide-react';
 import axios from 'axios';
+import { colors } from '@mui/material';
 
 const api = "http://localhost:3000/data"
 
-
+import FolderDeleteTwoToneIcon from '@mui/icons-material/FolderDeleteTwoTone';
 
 const About = () => {
   const openDialog = useRef(null);
   const openEditDialog = useRef(null);
 
   const [ data , setData ] = useState ( [] )
+
+  const [ deleteObj , setDeleteObj ] = useState ( [] )
 
   const [ inpAddName , setInpAddName ] = useState ( '' )
   const [ inpAddImg , setInpAddImg ] = useState ( '' )
@@ -23,6 +26,9 @@ const About = () => {
   const [ inpEditEmail , setInpEditEmail ] = useState ( '' )
   const [ inpEditPhone , setInpEditPhone ] = useState ( '' )
   const [ inpEditCity , setInpEditCity ] = useState ( '' )
+  const [ idx , setIdx ] = useState ( null )
+
+  const [ inpSearch , setInpSearch ] = useState ( '' )
 
 
   async function getApi () {
@@ -34,9 +40,6 @@ const About = () => {
     }
   }
 
-  // async function ChangeUser ( id ) { 
-  //   inpEditName = elem.name , inpEditImg = elem.img , inpEditEmail = elem.email, inpEditCity = elem.city , inpEditPhone = elem.phone
-  // }
 
   async function AddNewUser () {
     try {
@@ -50,12 +53,75 @@ const About = () => {
       })
       getApi(data)
       openDialog.current.close()
+      setInpAddCity = ""
+      setInpAddEmail = ""
+      setInpAddImg = ""
+      setInpAddName = ""
+      setInpAddPhone = ""
+      setInpAddImg = ""
     } catch (error) {
       console.error(error);
       
     }
   }
 
+  function deleteUsers(elem) {
+    setDeleteObj((prev) => {
+      const updatedSet = new Set(prev.map((item) => item.id)); // Store existing IDs in a Set
+      if (!updatedSet.has(elem.id)) {
+        return [...prev, elem]; // Add only if not already in the list
+      }
+      return prev; // Return the same array if the element already exists
+    });
+    console.log(deleteObj);
+  }
+
+  function deleteAllUsers ( ) {
+    data.forEach (( elem ) => {
+      deleteObj . forEach (( item ) => {
+        if ( elem == item ) {
+          axios . delete ( `${api}/${elem.id}` )
+          .then (( res ) => {
+            getApi ( res . data )
+          })
+        }
+      })
+    })
+  }
+
+  function CancelDeleteUsers () {
+    setDeleteObj ( [] )    
+  }
+  
+  
+  
+  function ChangeUser () {
+    openEditDialog.current.showModal()
+    data.forEach (( elem ) => {
+      if ( elem.id === idx ) {
+        setInpEditName ( elem . name )
+        setInpEditEmail ( elem . email )
+        setInpEditCity ( elem . city )
+        setInpEditPhone ( elem . phone )        
+        setInpEditImg ( elem . img )
+    }})
+  }
+
+  async function EditUser ( ) {
+    try {
+      const { data } = await axios.put ( `${api}/${idx}` , {
+        name : inpEditName,
+        email : inpEditEmail,
+        city : inpEditCity,
+        phone : inpEditPhone,
+        img : inpEditImg,
+      })
+      openEditDialog.current.close()
+      getApi ( data )
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
 
   useEffect (() => {
@@ -73,6 +139,11 @@ const About = () => {
         + Add New User
       </button>
 
+      <FolderDeleteTwoToneIcon onClick ={ ()=> deleteAllUsers () } fontSize='large' className='text-red-600' style={{ display: deleteObj.length > 0 ? "block" : "none" }} />
+        <button onClick = { () => CancelDeleteUsers ()} className = "bg-blue-500 p-[5px_20px] rounded-[5px]" style={{ display: deleteObj.length > 0 ? "block" : "none" }}>
+          Cancel
+        </button>
+
       {/* Search input */}
       <div className="flex items-center">
         <input
@@ -80,7 +151,7 @@ const About = () => {
           placeholder="Search..."
           className="px-5 py-2 text-lg text-gray-900 bg-white border-2 border-gray-300 rounded-lg shadow-md outline-none transition-all duration-300 ease-in-out focus:border-blue-500 focus:ring-2 focus:ring-blue-300 focus:shadow-xl"
         />
-        <Search className="text-gray-600 ml-[-35px]" size={20} />
+        <Search className="text-gray-600 ml-[-35px]" onChange={(event) => setInpSearch ( event . target . value )} size={20} />
       </div>
 
       {/* Dialog box */}
@@ -109,12 +180,19 @@ const About = () => {
     </div>
     <div className='flex flex-wrap gap-[5%]  px-[10%]'>
       
-    {data.map((elem) => (
+    {data.filter (( elem ) => {
+      return (
+        elem.name.toLowerCase().trim().includes(inpSearch.toLowerCase().trim()) || 
+        elem.email.toLowerCase().trim().includes(inpSearch.toLowerCase().trim()) ||
+        elem.city.toLowerCase().trim().includes(inpSearch.toLowerCase().trim()) ||
+        elem.phone.toLowerCase().trim().includes(inpSearch.toLowerCase().trim())
+      )
+    }).map((elem) => (
   <div key={elem.id} className="hover:translate-y-2 duration-150 ease-in cursor-pointer m-auto mb-[5%] flex w-full max-w-md bg-white rounded-xl shadow-lg overflow-hidden border border-gray-200">
    
-    <img src={elem.img} alt={elem.name + "'s Avatar"} className="w-[60%] h-72 object-cover" />
+    <img src={elem.img} onClick={()=> deleteUsers ( elem ) } alt={elem.name + "'s Avatar"} className="w-[60%] hover:opacity-70 duration-100 ease-in-out h-72 object-cover" />
     
-    <div className="p-6">
+    <div className="p-6 duration-1000 w-full" style={{ backgroundColor: deleteObj.includes(elem) ? "#bd0d0d" : "white" }}>
       <h2 className="text-xl font-semibold text-gray-800">{elem.name}</h2>
       <p className="text-gray-500 text-sm">{elem.email}</p>
       <p className="text-gray-700 mt-2">
@@ -123,7 +201,7 @@ const About = () => {
       <p className="text-gray-700">
         <span className="font-medium">Phone:</span> {elem.phone}
       </p>
-      <button onClick={()=> openEditDialog.current.showModal() } className="mt-4 px-5 py-2 bg-gradient-to-r from-blue-700 to-blue-900 text-white rounded-lg font-semibold shadow-md hover:opacity-90 transition">
+      <button onClick={()=> ChangeUser ( setIdx ( elem . id ) )  } className="mt-4 px-5 py-2 bg-gradient-to-r from-blue-700 to-blue-900 text-white rounded-lg font-semibold shadow-md hover:opacity-90 transition">
               Rename
       </button>
 
@@ -134,9 +212,9 @@ const About = () => {
   <div className="space-y-4">
     <input type="text" value={inpEditImg} onChange={()=> setInpEditImg(event.target.value)} className="w-full p-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-gold-500" />
     <input type="text" value={inpEditName} onChange={()=> setInpEditName(event.target.value)} placeholder="Full Name" className="w-full p-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-gold-500" />
-    <input type="email" value={inpEditEmail} onChange={()=> setInpAddEmail(event.target.value)} placeholder="Email" className="w-full p-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-gold-500" />
+    <input type="email" value={inpEditEmail} onChange={()=> setInpEditEmail(event.target.value)} placeholder="Email" className="w-full p-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-gold-500" />
     <input type="text" value={inpEditCity} onChange={()=> setInpEditCity(event.target.value)} placeholder="City" className="w-full p-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-gold-500" />
-    <input type="number" value={inpEditPhone} onChange={()=> setInpEditPhone(event.target.value)} placeholder="Phone Number" className="w-full p-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-gold-500" />
+    <input type="text" value={inpEditPhone} onChange={()=> setInpEditPhone(event.target.value)} placeholder="Phone Number" className="w-full p-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-gold-500" />
   </div>
 
   <div className="flex justify-end space-x-4 mt-6">
@@ -148,7 +226,7 @@ const About = () => {
     </button>
 
     <button 
-    onClick={()=> ChangeUser ( elem . id )  }
+    onClick={()=> EditUser () }
       className="px-5 py-2 bg-gradient-to-r from-blue-800 to-blue-500 text-white font-medium rounded-lg hover:from-blue-900 hover:to-blue-600 transition shadow-lg"
     >
       Save Changes
